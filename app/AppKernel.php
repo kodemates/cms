4c3c00dd857cac1bef8411984ed083363e2f01f3
Luciano Andrade <andrade.luciano@gmail.com>
2013-12-06 00:47:25 +0000

    FB Connect working, but logout whon't logout on the CMS.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\FacebookBundle\FOSFacebookBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
 
             new CMS\FrontendBundle\CMSFrontendBundle(),
         );
